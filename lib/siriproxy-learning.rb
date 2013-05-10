bb59d3d6d250196048cb4f33e83b6723c9d09ea7
Matthias Alker <diego-@gmx.de>
2013-05-10 09:54:07 +0200

    Test

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -15,13 +15,14 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   end
 
   listen_for /test it/i do
-  say "Joadsada"
-  svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "mar", :password=> "Bachelor4711" }
-
-
-svc.Pages.filter("Parent eq '0'")
-prod = svc.execute
-say "#{prod.to_json}"
+      say "Joadsada"
+      svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", 
+      { :username => "mar", :password=> "Bachelor4711" }
+    
+    
+    svc.Pages.filter("Parent eq '0'")
+    prod = svc.execute
+    say "#{prod.to_json}"
    request_completed
   end
 
