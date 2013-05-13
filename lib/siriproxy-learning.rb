bc8b24c40d8c58b3aba5925b7d9b2f9da371fe73
Matthias Alker <diego-@gmx.de>
2013-05-13 09:39:14 +0200

    Test

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -5,25 +5,28 @@ require 'ruby_odata'
 
 class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   def initialize(config)
-  @value = ""
-
-    #if you have custom configuration options, process them here!
+    @kopf_eintraege = ""
   end
 
   def start_query
   
   end
 
-  listen_for /test it/i do
-      say "Joadsfrasada"
-      svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", 
-      { :username => "mar", :password=> "Bachelor4711" }
+  listen_for /Alle Kopfeinträge/i do
+    say "Joadsfrasada"
+    svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", 
+    { :username => "mar", :password=> "Bachelor4711" }
+      
+    svc.Pages
+    @kopf_eintraege = svc.execute
+    @kopf_eintraege.each do |c|
+        say "#{c.Entryid}"
+    end
     
-      say svc
-    svc.Pages.filter("Parent eq '0'")
-    prod = svc.execute
-    say "#{prod.to_json}"
-   request_completed
+    request_completed
   end
+
+
+
 
 end
