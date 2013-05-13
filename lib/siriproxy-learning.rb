2e1c63dcc08a5b05d7c0922cbbb38d81c13a9c41
MatthiasTesting <diego-@gmx.de>
2013-05-13 16:17:32 +0300

    Update siriproxy-learning.rb

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -70,7 +70,7 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
 
        say "Detailinformationen zu " + page_id + " werden ermittelt!", spoken: "Checking"
        say "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV/Pages('#{page_id}')/GetDetails?sap-ds-debug=true"
-      @service = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV/Pages(#{page_id})/GetDetails?sap-ds-debug=true", { :username => "mar", :password=> "Bachelor4711" }
+      @service = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV/Pages('#{page_id}')/GetDetails?sap-ds-debug=true", { :username => "mar", :password=> "Bachelor4711" }
         #say @service
 
         #@detail_eintrag = @service.execute
