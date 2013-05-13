064b5d52fc70db41943113dd5a3ce51af36d6de7
Matthias Alker <diego-@gmx.de>
2013-05-13 11:12:47 +0200

    edas

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -12,13 +12,22 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   
   end
 
-  listen_for /Alle Kopfeintraege suchenn/i do
+  listen_for /Alle Kopfeintraege suchen/i do
     say "Es werden alle Kopfeintraege gesucht"
   
           svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "mar", :password=> "Bachelor4711" }
       
           svc.Pages
           @kopf_eintraege = svc.execute
+          
+          svc.Pages.count
+          count = svc.execute
+          
+          say "#{count}"
+          # wenn Count > 0 Einträge vorhanden,
+          # ansonsten Abbruch
+          say "Folgende Kopfeintraege stehen zur Verfuegung"
+
           @kopf_eintraege.each do |c|
               say "#{c.Name}"
           end
@@ -29,7 +38,10 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
 end
   
   listen_for /Detail zu (.*)/i do | page_name |
-    say "Detailinformationen zu " + page_name + "werden ermittelt!", spoken: "Checking"
+    say "Detailinformationen zu " + page_name + " werden ermittelt!", spoken: "Checking"
+    # wenn Parent false ist, dann abspielen
+    
+    # bzw. Has Content abprüfen
   end
 
 end
