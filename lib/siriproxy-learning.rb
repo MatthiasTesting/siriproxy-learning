900cff0e463558f284d503c3cdbc8d75a28c1829
Matthias Alker <diego-@gmx.de>
2013-05-13 09:49:10 +0200

    tre

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -13,20 +13,9 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   end
 
   listen_for /Alle Kopfeinträge/i do
-    say "Joadsfrasada"
-    svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", 
-    { :username => "mar", :password=> "Bachelor4711" }
-      
-    svc.Pages
-    @kopf_eintraege = svc.execute
-    @kopf_eintraege.each do |c|
-        say "#{c.Entryid}"
-    end
+    
     
     request_completed
   end
-
-
-
 
 end
