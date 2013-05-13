649debd18adeaa719742cf95beec7b81e6a6e7c3
Matthias Alker <diego-@gmx.de>
2013-05-13 10:05:27 +0200

    das

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -12,18 +12,33 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   
   end
 
-  listen_for /alle/i do
-    say "Joadsfrasada"
-    svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", 
-    { :username => "mar", :password=> "Bachelor4711" }
+  listen_for /Alle Kopfeintraege suchen/i do
+    say "Es werden alle Kopfeinträge gesucht"
+    Thread.new {
+          svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", 
+             { :username => "mar", :password=> "Bachelor4711" }
       
-    svc.Pages
-    @kopf_eintraege = svc.execute
-    @kopf_eintraege.each do |c|
-        say "#{c.Entryid}"
-    end
+          svc.Pages
+          @kopf_eintraege = svc.execute
+          @kopf_eintraege.each do |c|
+              say "#{c.Name}"
+          end
+          
+          request_completed
+            
+    }
+  end
+  
+  listen_for /Detail zu (.*)/i do | page_name |
+    say "Detailinformationen zu " + page_name + "werden ermittelt!", spoken: "Checking"
     
-    request_completed
+    Thread.new {
+      
+      
+      
+      
+    }
+    
   end
 
 end
