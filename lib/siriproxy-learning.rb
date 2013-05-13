37e8f1c61e5eeab4ed5f4da4d0b2c6808b7a1d04
Matthias Alker <diego-@gmx.de>
2013-05-13 10:13:15 +0200

    das

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -13,15 +13,15 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   end
 
   listen_for /Alle Kopfeintraege suchenn/i do
-    say "Es werden alle Kopfeinträge gesucht"
+    say "Es werden alle Kopfeintraege gesucht"
     Thread.new {
           svc = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "mar", :password=> "Bachelor4711" }
       
-          #svc.Pages
-          #@kopf_eintraege = svc.execute
-         # @kopf_eintraege.each do |c|
-          #    say "#{c.Name}"
-          #end
+          svc.Pages
+          @kopf_eintraege = svc.execute
+          @kopf_eintraege.each do |c|
+              say "#{c.Name}"
+          end
           
           request_completed
             
