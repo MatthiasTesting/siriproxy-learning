c0e313cb1935ad1fc7b88229365930e05c94137d
MatthiasTesting <diego-@gmx.de>
2013-05-14 15:48:47 +0300

    Update siriproxy-learning.rb

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -78,16 +78,17 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
         detail.each do |c|
         # Eigentlich fragen ob Content vorlesen oder subpages vorlesen und ggf. eine ebene runter gehne
             if c.Content == "" && c.Has_Subpages == true
+               say "Erste Bedinggun"
                # Ask if subpages should be shown 
-               @service.Pages("'#{page_id}'").expand('GetDetails').expand('GetDetails/GetSubpages')
-               detail = @service.execute
+               #@service.Pages("'#{page_id}'").expand('GetDetails').expand('GetDetails/GetSubpages')
+              # detail = @service.execute
 
-               detail.each do |b|
-                  say "#{b.Entryid}"
-               end 
+               #detail.each do |b|
+               #   say "#{b.Entryid}"
+              # end 
             else
                say "Content wird vorgelesen"
-               say "#{c.Content}"
+              # say "#{c.Content}"
             end
         end
        request_completed
