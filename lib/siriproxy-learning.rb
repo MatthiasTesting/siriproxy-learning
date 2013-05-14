9dc1848224ce12e4fbd0fc2d5a40e331954c33e4
MatthiasTesting <diego-@gmx.de>
2013-05-14 14:57:31 +0300

    Update siriproxy-learning.rb

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -72,7 +72,7 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
 
        start_connection
        
-       @service.Pages("'#{page_id}'").expand('GetDetails')
+       @service.Pages("'#{page_id}'").expand('GetDetails').links('GetSubpages')
        detail = @service.execute
        
        say "#{detail.to_json}"
