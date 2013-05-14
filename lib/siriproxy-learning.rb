34b6dcda8e5349b1f0b9a363d78c3bea52cace0a
MatthiasTesting <diego-@gmx.de>
2013-05-14 11:55:53 +0300

    Update siriproxy-learning.rb

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -72,7 +72,7 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
        #say "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV/Pages('#{page_id}')/GetDetails?sap-ds-debug=true"
        #@service.Pages.expand('GetDetails')
        start_connection
-       @service.Pages('00000002').links("GetDetails")
+       @service.FullPages('00000002')
        detail = @service.execute
        #say @service
 
