7c714325a615341b00412a04f8a975f0d1d9c318
MatthiasTesting <diego-@gmx.de>
2013-05-17 13:38:06 +0200

    Update siriproxy-learning.rb

--- lib/siriproxy-learning.rb
+++ lib/siriproxy-learning.rb
@@ -15,11 +15,11 @@ class SiriProxy::Plugin::Learning < SiriProxy::Plugin
   end
 
   def start_connection 
-        @service = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "mar", :password=> "Bachelor4711." }
+        @service = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "", :password=> "" }
   end
   
   def check_connection 
-        @service = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "mar", :password=> "Bachelor4711." }
+        @service = OData::Service.new "http://bfessfd.intern.itelligence.de:8000/sap/opu/odata/sap/ZLIST_SRV", { :username => "", :password=> "" }
    
         if @service.respond_to? :Pages == true
           say "Connection established"  
